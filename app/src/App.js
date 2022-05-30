3b81433f02e859a7429952ee3717f8e8cb3922a7
milliorn <scottmilliorn@gmail.com>
2022-05-30 15:52:21 -0700

    rng

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,41 @@
-import './App.css';
+import "./App.css";
+import equilibrium from "./images/image-equilibrium.jpg";
+import ethereum from "./images/icon-ethereum.svg";
+import clock from "./images/icon-clock.svg";
+import avatar from "./images/image-avatar.png";
 
 function App() {
+  let equilibriumRng = Math.floor(Math.random() * 255 + 1);
+  let daysRng = Math.floor(Math.random() * 5 + 2);
+  let coinRng = (Math.random()).toPrecision(3);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="card">
+      <img class="nft-card-image" src={equilibrium} alt="equilibrium preview" />
+      <div class="img-overlay">
+        <img src="./images/icon-view.svg" alt="view icon" />
+      </div>
+      <h2>Equilibrium #{equilibriumRng}</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div class="container">
+        <div class="price">
+          <img src={ethereum} alt="Eth icon" />
+          <div>{coinRng} ETH</div>
+        </div>
+        <div class="time">
+          <img src={clock} alt="time icon" />
+          <div>{daysRng} Days Left</div>
+        </div>
+      </div>
+      <div class="creator">
+        <img src={avatar} alt="avatar" />
+        <div>
+          Creation of <span>Lorem Ipsum</span>
+        </div>
+      </div>
     </div>
   );
 }
